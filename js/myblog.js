ddf3d12aeca1a6ccda443e90b5f99fae6b6acf8a
okchunggon <cndrhs107@gmail.com>
2017-12-29 17:12:05 +0900

    href jquery 추가

--- js/myblog.js
+++ js/myblog.js
@@ -6,4 +6,19 @@ $(document).ready(function(){
 		var id = $(this).attr('id');
 		alert("id :" + id);
 	});
+
+	$(".resume").on("click",function(){
+				/* 다른 사이트로 이동 */
+				// $(location).attr('href',"http://www.naver.com");
+				// location.href = "http://www.naver.com";
+				/* 다른 파일(html)로 이동 */
+				location.href = "./myBlog.resume.html";
+			});
+		$(".mainLogo").on("click",function(){
+				/* 다른 사이트로 이동 */
+				// $(location).attr('href',"http://www.naver.com");
+				// location.href = "http://www.naver.com";
+				/* 다른 파일(html)로 이동 */
+				location.href = "./myBlog.html";
+			});
 });
