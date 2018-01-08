9bf38c65e2aa267175e0751ce03565c7a749fd3e
okchunggon <cndrhs107@gmail.com>
2018-01-08 09:22:55 +0900

    Revert "새로운 JS파일 생성 (AJAX)"
    This reverts commit 4bd24f2dcd35cc6adf8e2f1bd142ff29fe2e7925.

--- js/myblog.js
+++ js/myblog.js
@@ -1,6 +1,29 @@
 $(document).ready(function(){
 
-
+	$.ajax({
+		type:"GET",
+		url :"http://localhost:8080/privateBlog/sample/getMenuList.do",
+		dataType: "jsonp",
+		error : function(data){
+			alert("수신 실패");
+		},
+		success: function(data){
+			console.log(data);
+			
+		
+			for(var i=0; i<4; i++){
+				/*console.log('data.menuList['+i+'],BM_NAME : '+ data.menuList[i].BM_NAME);
+				console.log('data.menuList['+i+'],BM_LEV : '+ data.menuList[i].BM_LEV);
+				console.log('data.menuList['+i+'],BM_STEP : '+ data.menuList[i].BM_STEP);
+				console.log('data.menuList['+i+'],ROWNUM : '+ data.menuList[i].ROWNUM);
+				console.log('data.menuList['+i+'],BM_REF : '+ data.menuList[i].BM_REF);
+				console.log('data.menuList['+i+'],BM_ISDEL : '+ data.menuList[i].BM_ISDEL);
+				console.log('data.menuList['+i+'],BM_IDNUM : '+ data.menuList[i].BM_IDNUM);*/
+				$(".menu").append('<li><a href="#" id="'+data.menuList[i].BM_IDNUM+'">'+data.menuList[i].BM_NAME+'</a></li>');
+			}
+			
+		}
+	});
 	// 클릭 가능하게끔
 	$(".menu a").click(function(){
 		var id = $(this).attr('id');
